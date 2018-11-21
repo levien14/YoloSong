4950ceb1460e7a9b2413001c11f665f6f8ba3837
LeVien <vienlvv14091998@gmail.com>
2018-11-21 23:38:32 +0700

    first commit

---
+++ Song_Category/Controllers/SongsController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Song_Category.Models;
+
+namespace Song_Category.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public SongsController(MyDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Songs
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Songs.ToListAsync());
+        }
+
+        // GET: Songs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Songs
+                .Include(s => s.SongCategories)
+                .ThenInclude(sc =>sc.Category)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            return View(song);
+        }
+
+        // GET: Songs/Create
+        public IActionResult Create()
+        {
+            var categories = _context.Categories.ToList();
+            ViewData["categories"] = categories;
+            return View();
+        }
+
+        // POST: Songs/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name")] Song song, int[] CategoriesId)
+        {
+            if (ModelState.IsValid)
+            {
+                foreach(var id in CategoriesId)
+                {
+                    var category = _context.Categories.Find(id);
+                    SongCategory songCategory = new SongCategory()
+                    {
+                        Category = category,
+                        Song = song
+                    };
+                    _context.Add(songCategory);
+                }
+                _context.Add(song);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(song);
+        }
+
+        // GET: Songs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Songs.FindAsync(id);
+            var category = _context.Categories.ToList();
+            ViewData["categories"] = category;
+            if (song == null)
+            {
+                return NotFound();
+            }
+            return View(song);
+        }
+
+        // POST: Songs/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] Song song, int[] CategoriesId)
+        {
+            if (id != song.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    foreach (var ID in CategoriesId)
+                    {
+                        var category = _context.Categories.Find(ID);
+                        var check = _context.SongCategories.Find(ID);
+                        SongCategory songCategory = new SongCategory()
+                        {
+                            Category = category,
+                            Song = song
+                        };
+                        _context.Remove(check);
+                        _context.Add(songCategory);
+                    }
+                    _context.Update(song);
+                    
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SongExists(song.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(song);
+        }
+
+        // GET: Songs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Songs
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            return View(song);
+        }
+
+        // POST: Songs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var song = await _context.Songs.FindAsync(id);
+            _context.Songs.Remove(song);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SongExists(int id)
+        {
+            return _context.Songs.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Song_Category/Models/MyDbContext.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Song_Category.Models
+{
+    public class MyDbContext : DbContext
+    {
+        public DbSet<Song> Songs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<SongCategory> SongCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SongCategory;Trusted_Connection=True;MultipleActiveResultSets=true");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SongCategory>()
+                .HasKey(sc => new { sc.SongId, sc.CategoryId });
+        }
+    }
+
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<SongCategory> SongCategories { get; set; }
+    }
+    public class  Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<SongCategory> SongCategories { get; set; }
+    }
+    public class SongCategory
+    {
+        public int SongId { get; set; }
+        public int CategoryId { get; set; }
+
+        public Song Song { get; set; }
+        public Category Category { get; set; }
+    }
+}
